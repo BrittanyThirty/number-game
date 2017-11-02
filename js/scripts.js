cc5a7b66a619fd3478b352234700e931afe765bc
Qianqian Hu <qianhu@pindous.com>
2017-11-02 12:54:04 -0700

    logic works

--- js/scripts.js
+++ js/scripts.js
@@ -1,60 +1,90 @@
 // business logic
 var devisibleByThree = function(number) {
-  if (number % 3 === 0) {
+  if (number % 3 === 0)
     return true;
-  }
-  else {
   return false;
-}
 };
-// var containingOne = function(number) {
-//   while (number > 0) {
-//     if (number % 10 === 1) {
-//       return true;
-//     }
-//     number = Math.floor(number / 10 );
-//   }
-//   else {
-//   return false;
-//   }
-// };
-// var containingZero = function(number) {
-//   while (number > 0) {
-//     if (number % 10 === 0) {
-//       return true;
-//     }
-//     number = Math.floor(number / 10 );
+// var stringNumber = number.toString();
+// var numberContainingOne = function(number) {
+//   if (number % 10 === 1) {
+//     return true;
 //   }
 //   else {
 //   return false;
 // }
 // };
+// var numberContainingZero = function(number) {
+//   if (number % 10 === 0) {
+//     return true;
+//   }
+//   else {
+//   return false;
+// }
+// };
+
+var numberContainingOneString = function(numberInt) {
+    var numberString = numberInt.toString();
+    if (numberString.includes("1"))
+      return true;
+    return false;
+}
+
+var numberContainingZeroString = function(numberInt) {
+    var numberString = numberInt.toString();
+    if (numberString.includes("0"))
+      return true;
+    return false;
+}
+
 // user interface logic
 $(document).ready(function() {
   $("form#number-game").submit(function(event) {
     event.preventDefault();
-    // var numberItselfInput = $("#numberItself").val();
-    // $(".numberItself").text(nunberItselfInput);
-    var number = parseInt($("#number").val());
-    var result = devisibleByThree(number);
-    // $("#numberItself").text(result);
-    // var result2 = containingOne(number);
-    // $("#numberContainingOne").text(result2);
-    // var result3 = containingZero(number);
-    // $("#numberContainingZero").text(result3);
 
-    if (result === true) {
-    $("#numberDivisibleByThree").show();
-  }
-    // else if (result2 === true) {
-    //   $("#numberContainingOne").show();
-    // }
-    // else if (result3 === true) {
-    //   $("#numberContainingZero").show();
-    // }
+    //var number = parseInt($("#number").val());
+
+    //var result1 = devisibleByThree(number);
+    //var result2 = numberContainingOne(number);
+    //var result3 = numberContainingZero(number);
+
+    var numberIntBig = parseInt($("#number").val());
+    var result1 = devisibleByThree(numberIntBig);
+    var result2 = numberContainingOneString(numberIntBig);
+    var result3 = numberContainingZeroString(numberIntBig);
+
+    $("form#number-game").each(function() {
+      this.reset();
+    });
+
+    // hide all the potential results with class name resultDisplay
+    $(".resultDisplay").hide();
+
+    if (result1 === true && result2 === true && result3 === true) {
+      $("#numberContainingZeroOneDThree").show();
+    }
+    else if (result1 === true && result2 === true && result3 === false) {
+      $("#numberContainingOneDThree").show();
+    }
+    else if (result3 === true && result2 === false && result3 === true) {
+      $("#numberContainingZeroDThree").show();
+    }
+    else if (result3 ===false && result2 === true && result3 === true) {
+      $("#numberContainingOneZero").show();
+    }
+    else if (result3 ===false && result2 === true && result3 === false) {
+      $("#numberContainingOne").show();
+    }
+    else if (result3 ===false && result2 === false && result3 === true) {
+      $("#numberContainingZero").show();
+    }
     else {
+      var numbersString = "";
+      for (index = 0; index <= numberIntBig; index += 1) {
+          //numbersString = numbersString.concat(index + ",");
+          numbersString = numbersString.concat(index.toString() + ", ");
+      }
+      $("#numberItself").text(numbersString);
       $("#numberItself").show();
-  }
-
+    }
 });
 });
